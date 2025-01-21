18539d41b29cd76b77ed145b42dcc43b4367accb
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-01-21 14:02:21 -0500

    How fast are changes reflected?

--- src/App.tsx
+++ src/App.tsx
@@ -80,7 +80,7 @@ function App() {
 
       <div className="card">
         <p>{eData}</p>
-        <p>Click Me</p>
+        <p>Click Me!</p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
