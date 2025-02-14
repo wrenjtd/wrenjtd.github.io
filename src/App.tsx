58fa256583730c9d5c826dc85d0228a6ac7331b1
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-02-14 11:33:01 -0500

    Back to basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,68 @@
-import { useEffect, useState } from 'react'
+import { useEffect} from 'react'
 import './App.css'
-import { OAuth2Client, generateCodeVerifier } from '@badgateway/oauth2-client';
+import { OAuthError } from './errors';
+// import { OAuthResponse, TravelerConfig } from './type-definitions/additons';
+// import ky from 'ky';
+
+
+
 
 
 
 function App() {
   //const [count, setCount] = useState(0);
-  const some = 48832;
- 
-  const [authResp, setAuthResp] = useState({});
-  
-  const auth_endpoint = `https://www.bungie.net/en/OAuth/Authorize?client_id=${some}&response_type=code`;
-  const base_url = "https://www.bungie.net/en/OAuth/";
-  const redirect_uri_2 = "https://wrenjtd.github.io/";
-  //const redirect_uri = "localhost:5173/";
-  const token_url = "/token/";
-  const auth_url = "/authorize";
-  
-  
-  
-  
-  const additionalHeaders = { 'X-API-Key': import.meta.env.VITE_BUNGIE_API_KEY, 'origin': 'https://wrenjtd.github.io/' };
+  // const some = 48832;
 
- 
+  //const [authResp, setAuthResp] = useState({});
+
+  const auth_endpoint = "https://www.bungie.net/en/OAuth/Authorize?client_id=" + import.meta.env.VITE_BUNGIE_API_CLIENT_ID + "&response_type=code";
+  //const base_url = "https://www.bungie.net/en/OAuth/";
+  //const redirect_uri = "https://wrenjtd.github.io/";
+  //const redirect_uri = "localhost:5173/";
+  //const token_url = "/token/";
+  //const auth_url = "/authorize";
+
+
+
+
+
+
+  //const additionalHeaders = { 'X-API-Key': import.meta.env.VITE_BUNGIE_API_KEY, 'origin': redirect_uri };
+
+
   const openInNewTab = (url: string) => {
     const newWindow = window.open(url, '_self', 'noopener,noreferrer')
     if (newWindow) newWindow.opener = null
   }
 
 
-  //Start new session
-  const session = new OAuth2Client({
-    server: base_url,
-    clientId: import.meta.env.VITE_BUNGIE_API_CLIENT_ID,
-    clientSecret: import.meta.env.VITE_BUNGIE_API_SECRET,
-    tokenEndpoint: token_url,
-    authorizationEndpoint: auth_url
+ 
 
-  });
+  const generateOAuthURL = (oauthClientId: string): string => {
+    if (oauthClientId !== undefined) {
+      return `https://www.bungie.net/en/OAuth/Authorize?client_id=${oauthClientId}&response_type=code`;
+    } else {
+      throw new OAuthError('You did not specify a OAuth client Id');
+    }
+  }
 
+
+
+  
+
+  
 
   //Authorization request to authorize user
-  const getData = async () => {
-    const codeVerifier = await generateCodeVerifier();
-    const authorizationResponse = await fetch(auth_endpoint, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'authorization': 'Basic ' + btoa(import.meta.env.VITE_BUNGIE_API_CLIENT_ID + ":" + import.meta.env.VITE_BUNGIE_API_CLIENT_SECRET),
-        'grant_type': 'authorization_code&code=f8ab7a7c1954cd44854793c400315ae6',
-        ...additionalHeaders
-
-    }, mode: 'no-cors'
-    
- })
-    
-    
-    // //exchange code for token
-    setAuthResp(authorizationResponse);
-
-    console.log(authResp);
-    session.authorizationCode.getToken({
-      code: "?code=f8ab7a7c1954cd44854793c400315ae6",
-      redirectUri: redirect_uri_2,
-      codeVerifier,
-    });
-    
-}
+  // const getData = async () => {
+  //   fetch(auth_url)
+  //   console.log("Hello");
+  // }
 
 
 
   useEffect(() => {
 
-    
+
 
   }, [])
 
@@ -80,11 +70,13 @@ function App() {
     <>
 
       <div className="card">
-        
+
         <button onClick={() => openInNewTab(auth_endpoint)}>Login to Bungie.NET
         </button>
 
-        <button onClick={() => getData()}>Don't click me yet!
+       
+
+        <button onClick={() => generateOAuthURL(import.meta.env.VITE_BUNGIE_API_CLIENT_ID)}>Start here!
         </button>
 
       </div>
