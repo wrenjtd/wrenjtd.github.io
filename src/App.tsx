65320264924b62d03fe71304641f70773efb976e
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-02-15 13:32:04 -0500

    grant_type change

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,7 @@ function App() {
       body: new URLSearchParams({
         'client_id': import.meta.env.VITE_BUNGIE_CLIENT_ID,
         'client_secret': import.meta.env.VITE_BUNGIE_CLIENT_SECRET,
-        'grant_type': "grant_type=authorization_code&code="+code+"&client_id="+import.meta.env.VITE_BUNGIE_CLIENT_ID+"&client_secret="+import.meta.env.VITE_BUNGIE_CLIENT_SECRET,
+        'grant_type': "authorization_code",
         'code': code
       }).toString()
     }).then(function(response) {
