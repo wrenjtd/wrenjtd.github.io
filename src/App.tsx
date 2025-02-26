0ac4eafe4f1a449fc64c9ac4c1733bb6e1f4c594
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-02-26 13:05:24 -0500

    Adding header and sidebar. Brace for impact!

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import '../src/assets/css/App.css';
 import { BrowserRouter } from 'react-router-dom';
 import MainBoxComponent from './components/UI_Sections/MainBox.component';
+import HeaderContentComponent from './components/UI/HeaderContent.component';
+import SidebarBoxComponent from './components/UI_Sections/SidebarBox.component';
 
 
 
@@ -16,10 +18,10 @@ function App() {
     <React.Fragment>
     <BrowserRouter>
     
-    
+    <HeaderContentComponent></HeaderContentComponent>
     <div id="main_flex_container">
     
-      
+      <SidebarBoxComponent></SidebarBoxComponent>
       <MainBoxComponent></MainBoxComponent>
       </div>
     </BrowserRouter>
