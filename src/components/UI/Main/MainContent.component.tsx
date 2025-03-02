cfb4c90df1e60e6e0dbe47bea066601605954634
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-02 16:36:39 -0500

    contin

--- src/components/UI/Main/MainContent.component.tsx
+++ src/components/UI/Main/MainContent.component.tsx
@@ -12,7 +12,7 @@ const MainContentComponent = ({ membershipData }: { membershipData: ServerRespon
 
   return (
 
-    <div className="h-[100%] max-h-xl">
+    <div className="max-h-full">
 
       <header className="shadow-sm">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
