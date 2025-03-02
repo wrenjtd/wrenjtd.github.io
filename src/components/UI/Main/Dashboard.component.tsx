7a2e187df9f0133e1122000faeab8653123c2b5f
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-02 13:54:44 -0500

    Adding header component

--- src/components/UI/Main/Dashboard.component.tsx
+++ src/components/UI/Main/Dashboard.component.tsx
@@ -1,14 +1,15 @@
+import HeaderContentComponent from "../Header/HeaderContent.component";
 
 export default function Dashboard() {
-  
-  
+
+
 
 
   return (
     <>
       <div className="min-h-full">
-       
 
+        <HeaderContentComponent></HeaderContentComponent>
         <header className="bg-white shadow-sm">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
