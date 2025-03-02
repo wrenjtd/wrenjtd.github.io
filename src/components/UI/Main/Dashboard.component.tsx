9f012c8cf6f6db9d3a66de3efdf5fce00a9fee01
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-02 15:59:04 -0500

    Unhide leftbar

--- src/components/UI/Main/Dashboard.component.tsx
+++ src/components/UI/Main/Dashboard.component.tsx
@@ -19,8 +19,8 @@ export default function Dashboard() {
         <main>
           { /* mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 */}
           <div className="">
-            {membershipData && <LeftbarBoxComponent membershipData={membershipData} />
-            }
+            <LeftbarBoxComponent membershipData={membershipData} />
+            
           </div>
         </main>
       </div>
