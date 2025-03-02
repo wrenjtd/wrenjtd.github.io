d81a3efbca3ef6ef320975df8a63253a72810842
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-02 14:06:41 -0500

    Leftbox dimensions

--- src/components/UI/Leftbar/LeftbarBox.component.tsx
+++ src/components/UI/Leftbar/LeftbarBox.component.tsx
@@ -24,7 +24,7 @@ const LeftbarBoxComponent: React.FC = () => {
 
 
 
-<div className="flex bg-gray-100 text-gray-900">
+<div className="flex bg-gray-100 text-gray-900 w-[100%] h-[100%]">
   <aside className="flex h-screen w-20 flex-col items-center border-r border-gray-200 bg-white">
     <div className="flex h-[4.5rem] w-full items-center justify-center border-b border-gray-200 p-2">
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThsapwuIZ2JPUVRaWSoX_xoEIOHWxneY7EupS8gsFriA&s" />
