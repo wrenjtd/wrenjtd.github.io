e457587a88eb2a2cf4b75d1347b60a5bffe8275b
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-11 23:47:39 -0400

    Fix Linting error

--- src/components/UI/HeaderContent.component.tsx
+++ src/components/UI/HeaderContent.component.tsx
@@ -11,12 +11,7 @@ import { ApiResponse } from '../../type-definitions/additons';
 
 
 
-const user = {
-    name: 'Tom Cook',
-    email: 'tom@example.com',
-    imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+
 const navigation = [
     { name: 'Dashboard', href: '#', current: false },
     { name: 'Team', href: '#', current: false },
@@ -169,8 +164,8 @@ const HeaderContentComponent = ({membershipData}:{membershipData:ServerResponse<
                                 />
                         </div>
                         <div className="ml-3">
-                            <div className="text-base/5 font-medium text-white">{user.name}</div>
-                            <div className="text-sm font-medium text-gray-400">{user.email}</div>
+                            <div className="text-base/5 font-medium text-white">{membershipData && membershipData.Response.bungieNetUser.membershipId}</div>
+                            <div className="text-sm font-medium text-gray-400">{membershipData && membershipData.Response.bungieNetUser.displayName}</div>
                         </div>
                         <button
                             type="button"
