6d4063915834ad91d7b2fa78fa24d0d66b00b9b1
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-14 18:33:05 -0400

    doubting intellisense

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,7 @@ function App() {
   useEffect(() => {
     if (userCharacterProfiles) {
       traveler.destiny2.getDestinyEntityDefinition(TypeDefinition.DestinyInventoryItemDefinition, userCharacterProfiles.Response.characterEquipment.data[Object.keys(userCharacterProfiles.Response.characterEquipment.data)[0]].items[0].itemHash).then(response => {
-            console.log(response.Response);
+            console.log(response.Response.displayProperties.name);
           })
     }
     
