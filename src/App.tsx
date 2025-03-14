13e90a0299e91402e51a55a275e26a18e1fc356e
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-14 14:47:30 -0400

    Tracing

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ function App() {
 
   useEffect(() => {
     if (userCharacterProfiles) {
-      console.log(userCharacterProfiles.Response.CharacterEquipment.data[0]);
+      console.log(userCharacterProfiles.Response.CharacterEquipment);
       // let temp = traveler.destiny2.getDestinyEntityDefinition(TypeDefinition.DestinyInventoryItemDefinition, userCharacterProfiles.Response.CharacterEquipment.data[Object.keys(userCharacterProfiles.Response.CharacterEquipment.data)[0]].items[0].itemHash);
 
       // console.log(temp);
