02ad747709f4c7d0cfbd3827a23218acdb18c767
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-14 19:02:20 -0400

    signal trace

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
 
   const [bungieMembershipData, setBungieMembershipData] = useState<any>();
   const [userCharacterProfiles, setUserCharacterProfiles] = useState<any>();
-  // const [userCharacterEquipment, setUserCharacterEquipment] = useState<any>();
+  const [userCharacterEquipment, setUserCharacterEquipment] = useState<any>();
 
 
   //Checks if the authorization code is in the URL and if it is, it gets the access token
@@ -79,15 +79,15 @@ function App() {
   useEffect(() => {
     if (userCharacterProfiles) {
       traveler.destiny2.getDestinyEntityDefinition(TypeDefinition.DestinyInventoryItemDefinition, userCharacterProfiles.Response.characterEquipment.data[Object.keys(userCharacterProfiles.Response.characterEquipment.data)[0]].items[0].itemHash).then(response => {
-            console.log(response.Response);
+            setUserCharacterEquipment(response.Response);
           })
     }
   },[userCharacterProfiles])
 
-  // useEffect(() => {
-  //   console.log(userCharacterEquipment.Response.displayProperties.name);
-  // }
-  // ,[userCharacterEquipment])
+  useEffect(() => {
+    console.log(userCharacterEquipment.displayProperties.name);
+  }
+  ,[userCharacterEquipment])
 
 
   return (
