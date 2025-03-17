6ba399b2661acf391d9a514c179eb7e14d1343a4
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-17 19:22:54 -0400

    ""

--- src/App.tsx
+++ src/App.tsx
@@ -87,7 +87,6 @@ function App() {
 
   useEffect(() => {
     if (userCharacterProfiles) {
-      console.log(userCharacterProfiles.Response.characterEquipment.data[0].items)
       
       traveler.destiny2.getDestinyEntityDefinition(TypeDefinition.DestinyInventoryItemDefinition, userCharacterProfiles.Response.characterEquipment.data[Object.keys(userCharacterProfiles.Response.characterEquipment.data)[0]].items[0].itemHash.toString()).then(response => {
         setUserCharacterEquipment(response);
