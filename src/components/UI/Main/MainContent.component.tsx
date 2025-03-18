3ca369862ed12484a70081c4084b0dc711ddaed9
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-18 17:18:49 -0400

    break, right?

--- src/components/UI/Main/MainContent.component.tsx
+++ src/components/UI/Main/MainContent.component.tsx
@@ -43,8 +43,8 @@ const MainContentComponent = () => {
 
             <div className="mt-4">
 
-              {equipment && <p className="text-sm text-pretty text-gray-500">
-                {/* {equipment[1].Response.displayProperties.name} */}
+              {equipment[1] && <p className="text-sm text-pretty text-gray-500">
+                {equipment[1].Response.displayProperties.name}
                 
                 {/* <img src={`https://www.bungie.net/${equipment[1].Response.displayProperties.icon}`} alt="weapon icon" /> */}
               </p>}
