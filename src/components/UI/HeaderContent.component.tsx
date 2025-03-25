b9d8361822243d66d13aafeca802220877594d01
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-25 12:48:17 -0400

    "" Again

--- src/components/UI/HeaderContent.component.tsx
+++ src/components/UI/HeaderContent.component.tsx
@@ -54,84 +54,86 @@ const HeaderContentComponent = () => {
 
         <Disclosure as="nav" className="bg-transparent">
             <div className="mr-5 ml-5">
-                <div className="flex h-16 items-center justify-between">
-                <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
-                <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
-                    <div className="flex items-center">
-                        <div className="shrink-0">
-                            {/* This is the site logo */}
-                            <Link to="/">
-                                <img
-                                    alt="Your Company"
-                                    src={logo}
-                                    className="size-7"
-                                />
-                            </Link>
-                        </div>
-                        <div className="hidden md:block">
-                            <div className="ml-10 flex items-baseline space-x-4">
-                                {navigation.map((item) => (
-                                    <a
-                                        key={item.name}
-                                        href={item.href}
-                                        aria-current={item.current ? 'page' : undefined}
-                                        className={classNames(
-                                            item.current ? 'bg-transparent text-gray-100' : 'text-gray-700 hover:bg-gray-300 hover:text-gray-100',
-                                            'rounded-md px-3 py-2 text-sm font-medium',
-                                        )}
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))}
+                <div className="flex h-16 items-center ">
+                    <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+                    <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+                    <div className="flex-1 flex justify-between items-center">
+                        <div className="flex items-center">
+                            <div className="shrink-0">
+                                {/* This is the site logo */}
+                                <Link to="/">
+                                    <img
+                                        alt="Your Company"
+                                        src={logo}
+                                        className="size-7"
+                                    />
+                                </Link>
+                            </div>
+                            <div className="hidden md:block">
+                                <div className="ml-10 flex items-baseline space-x-4">
+                                    {navigation.map((item) => (
+                                        <a
+                                            key={item.name}
+                                            href={item.href}
+                                            aria-current={item.current ? 'page' : undefined}
+                                            className={classNames(
+                                                item.current ? 'bg-transparent text-gray-100' : 'text-gray-700 hover:bg-gray-300 hover:text-gray-100',
+                                                'rounded-md px-3 py-2 text-sm font-medium',
+                                            )}
+                                        >
+                                            {item.name}
+                                        </a>
+                                    ))}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="hidden md:block">
-                        <div className="ml-4 flex items-center md:ml-6">
-                            <button
-                                type="button"
-                                className="relative rounded-full bg-transparent p-1 text-gray-400 focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
-                            >
-                                <span className="absolute -inset-1.5" />
-                                <span className="sr-only">View notifications</span>
-                                <BellIcon aria-hidden="true" className="size-6" />
-                            </button>
-
-                            {/* Profile dropdown */}
-                            <Menu as="div" className="relative ml-3">
-                                <div>
-                                    <MenuButton className="relative flex max-w-xs items-center rounded-full bg-transparent text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                                        <span className="absolute -inset-1.5" />
-                                        <span className="sr-only">Open user menu</span>
-                                        <img alt="" src={settings_gear} className="size-6 " />
-                                    </MenuButton>
-                                </div>
-                                <MenuItems
-                                    transition
-                                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-transparent py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                        <div className="hidden md:block">
+                            <div className="ml-4 flex items-center md:ml-6">
+                                <button
+                                    type="button"
+                                    className="relative rounded-full bg-transparent p-1 text-gray-400 focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
                                 >
-                                    {userNavigation.map((item) => (
-                                        <MenuItem key={item.name}>
-                                            <button
-                                                onClick={item.onClick}
-                                                className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                                            >
-                                                {item.name}
-                                            </button>
-                                        </MenuItem>
-                                    ))}
-                                </MenuItems>
-                            </Menu>
+                                    <span className="absolute -inset-1.5" />
+                                    <span className="sr-only">View notifications</span>
+                                    <BellIcon aria-hidden="true" className="size-6" />
+                                </button>
+
+                                {/* Profile dropdown */}
+                                <Menu as="div" className="relative ml-3">
+                                    <div>
+                                        <MenuButton className="relative flex max-w-xs items-center rounded-full bg-transparent text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                                            <span className="absolute -inset-1.5" />
+                                            <span className="sr-only">Open user menu</span>
+                                            <img alt="" src={settings_gear} className="size-6 " />
+                                        </MenuButton>
+                                    </div>
+                                    <MenuItems
+                                        transition
+                                        className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-transparent py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                                    >
+                                        {userNavigation.map((item) => (
+                                            <MenuItem key={item.name}>
+                                                <button
+                                                    onClick={item.onClick}
+                                                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                                                >
+                                                    {item.name}
+                                                </button>
+                                            </MenuItem>
+                                        ))}
+                                    </MenuItems>
+                                </Menu>
+                            </div>
                         </div>
-                    </div>
-                    <div className="-mr-2 flex md:hidden">
-                        {/* Mobile menu button */}
-                        <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-200 p-2 text-gray-600 hover:bg-gray-300 hover:text-gray-100 focus:ring-2 focus:ring-gray-100 focus:ring-offset-2 focus:ring-offset-gray-200 focus:outline-hidden">
-                            <span className="absolute -inset-0.5" />
-                            <span className="sr-only">Open main menu</span>
-                            <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
-                            <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
-                        </DisclosureButton>
+                        <div className="-mr-2 flex md:hidden">
+                            {/* Mobile menu button */}
+                            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-200 p-2 text-gray-600 hover:bg-gray-300 hover:text-gray-100 focus:ring-2 focus:ring-gray-100 focus:ring-offset-2 focus:ring-offset-gray-200 focus:outline-hidden">
+                                <span className="absolute -inset-0.5" />
+                                <span className="sr-only">Open main menu</span>
+                                <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+                                <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+                            </DisclosureButton>
+                        </div>
                     </div>
                 </div>
             </div>
