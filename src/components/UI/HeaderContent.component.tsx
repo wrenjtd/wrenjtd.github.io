f6042fcfc61f0fd4497b0a06c4309713728cd502
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-25 12:40:55 -0400

    ""

--- src/components/UI/HeaderContent.component.tsx
+++ src/components/UI/HeaderContent.component.tsx
@@ -53,7 +53,7 @@ const HeaderContentComponent = () => {
     return (
 
         <Disclosure as="nav" className="bg-transparent">
-            <div className="mr-[20px] ml-[20px]">
+            <div className="mr-5 ml-5">
                 <div className="flex h-16 items-center justify-between">
                 <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
                 <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
