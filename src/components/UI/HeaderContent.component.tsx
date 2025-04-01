645b84d5eb17a059fefe0161e0e6d61a87c0b480
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-03-31 20:17:47 -0400

    Does this work?

--- src/components/UI/HeaderContent.component.tsx
+++ src/components/UI/HeaderContent.component.tsx
@@ -65,7 +65,7 @@ const HeaderContentComponent = () => {
                                     <img
                                         alt="Your Company"
                                         src={logo}
-                                        className="size-9"
+                                        className="size-15"
                                     />
                                 </Link>
                             </div>
--- src/components/UI/Main/MainContent.component.tsx
+++ src/components/UI/Main/MainContent.component.tsx
@@ -22,34 +22,35 @@ const MainContentComponent = () => {
 
   return (
     <article className=" bg-gray-800 p-4">
-     { user.bungieMembershipData && <div className="flex items-center gap-4">
+      {user.bungieMembershipData &&
 
+        <div className="flex items-center gap-4">
 
+          <div>
+            <div className="flex items-center gap-2">
+              <img src={`https://www.bungie.net/${user.bungieMembershipData.Response.bungieNetUser.profilePicturePath}`} alt="avatar" className="w-12 h-12 rounded-full" />
+              <h3 className="text-lg font-medium text-white">{user.bungieMembershipData && user.bungieMembershipData.Response.bungieNetUser.uniqueName}</h3>
+            </div>
 
-        <div>
-          <div className="flex items-center gap-2">
-            <img src={`https://www.bungie.net/${user.bungieMembershipData.Response.bungieNetUser.profilePicturePath}`} alt="avatar" className="w-12 h-12 rounded-full" />
-            <h3 className="text-lg font-medium text-white">{user.bungieMembershipData && user.bungieMembershipData.Response.bungieNetUser.uniqueName}</h3>
+            <div className="flow-root">
+              <ul className="-m-1 flex flex-wrap">
+                <li className="p-1 leading-none">
+                  <a href="#" className="text-xs font-medium text-gray-300"> Twitter </a>
+                </li>
+
+                <li className="p-1 leading-none">
+                  <a href="#" className="text-xs font-medium text-gray-300"> GitHub </a>
+                </li>
+
+                <li className="p-1 leading-none">
+                  <a href="#" className="text-xs font-medium text-gray-300">Website</a>
+                </li>
+              </ul>
+            </div>
           </div>
 
-          <div className="flow-root">
-            <ul className="-m-1 flex flex-wrap">
-              <li className="p-1 leading-none">
-                <a href="#" className="text-xs font-medium text-gray-300"> Twitter </a>
-              </li>
-
-              <li className="p-1 leading-none">
-                <a href="#" className="text-xs font-medium text-gray-300"> GitHub </a>
-              </li>
-
-              <li className="p-1 leading-none">
-                <a href="#" className="text-xs font-medium text-gray-300">Website</a>
-              </li>
-            </ul>
-          </div>
         </div>
-
-      </div>}
+        }
       <div>
         <ul className="mt-4 space-y-2">
           {equipment[1] && <li>
