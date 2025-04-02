6b72e6adaa8f50e8e32a95742537c6f48c9f7657
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-04-01 21:04:40 -0400

    Added Listbox

--- src/components/UI/Main/MainContent.component.tsx
+++ src/components/UI/Main/MainContent.component.tsx
@@ -2,7 +2,9 @@ import { useContext, useEffect, useState } from "react";
 import { BungieMembershipDataContext, contextType } from "../../../App";
 import { DestinyInventoryItemDefinition } from "../../../type-definitions/destiny2/interfaces";
 import { ServerResponse } from "../../../type-definitions/common";
-import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react';
+import { Label, Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/16/solid'
+
 
 const dungeons = [
   { id: 1, name: 'Durward Reynolds', unavailable: false },
@@ -19,7 +21,7 @@ const MainContentComponent = () => {
     ...membershipData as contextType
   }
   const [equipment, setEquipment] = useState<ServerResponse<DestinyInventoryItemDefinition>[]>([]);
-  const [selectedPerson, setSelectedPerson] = useState(dungeons[0])
+  const [selectedDungeon, setSelectedDungeon] = useState(dungeons[0])
 
   useEffect(() => {
     setEquipment(prev => [...prev, user.userEquipmentItems])
@@ -32,19 +34,37 @@ const MainContentComponent = () => {
 
     <article className=" bg-gray-800 p-4">
 
-<Listbox value={selectedPerson} onChange={setSelectedPerson}>
-      <ListboxButton>{selectedPerson.name}</ListboxButton>
-      <ListboxOptions>
-        {dungeons.map((dungeon) => (
-          <ListboxOption
-            key={dungeon.id}
-            value={dungeon}
-            disabled={dungeon.unavailable}
-          >
-            {dungeon.name}
-          </ListboxOption>
-        ))}
-      </ListboxOptions>
+<Listbox value={selectedDungeon} onChange={setSelectedDungeon}>
+      <Label className="block text-sm/6 font-medium text-gray-900">Assigned to</Label>
+      <div className="relative mt-2">
+        <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+          <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
+            <span className="block truncate">{selectedDungeon.name}</span>
+          </span>
+          <ChevronDownIcon
+            aria-hidden="true"
+            className="col-start-1 row-start-1 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+          />
+        </ListboxButton>
+
+        <ListboxOptions
+          transition
+          className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base ring-1 shadow-lg ring-black/5 focus:outline-hidden data-leave:transition data-leave:duration-100 data-leave:ease-in data-closed:data-leave:opacity-0 sm:text-sm"
+        >
+          {dungeons.map((dungeon) => (
+            <ListboxOption
+              key={dungeon.id}
+              value={dungeon}
+              className="group relative cursor-default py-2 pr-9 pl-3 text-gray-900 select-none data-focus:bg-indigo-600 data-focus:text-white data-focus:outline-hidden"
+            >
+              <div className="flex items-center">
+                <span className="ml-3 block truncate font-normal group-data-selected:font-semibold">{dungeon.name}</span>
+              </div>
+
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </div>
     </Listbox>
 
       {user.bungieMembershipData &&
