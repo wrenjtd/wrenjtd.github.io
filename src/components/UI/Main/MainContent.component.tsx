4b328dbbe421cd86f6a939e2eaedc2eb68625bf6
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-04-02 15:18:59 -0400

    bg-white change

--- src/components/UI/Main/MainContent.component.tsx
+++ src/components/UI/Main/MainContent.component.tsx
@@ -27,7 +27,7 @@ const MainContentComponent = () => {
     <article className=" bg-gray-800 p-4">
 
       <Listbox value={selectedDungeon} onChange={setSelectedDungeon}>
-        <Label className="block text-sm/6 font-medium text-gray-900">Assigned to</Label>
+        <Label className="block text-sm/6 font-medium text-white">Dungeons</Label>
         <div className="relative mt-2">
           <ListboxButton className="grid w-lg cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
             <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
