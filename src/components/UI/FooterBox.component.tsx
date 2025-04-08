4c8b3fc92ea9ba1de67f3cd758dba0142dc30729
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-04-08 19:09:02 -0400

    ""

--- src/components/UI/FooterBox.component.tsx
+++ src/components/UI/FooterBox.component.tsx
@@ -7,7 +7,7 @@ const FooterBoxComponent: React.FC = () => {
 
     return (
         
-            <div className="text-center text-gray-100 align-bottom">
+            <div className="text-center text-gray-100 items-end">
                 <p>Copyright 2025. All rights reserved</p>
             </div>
             
