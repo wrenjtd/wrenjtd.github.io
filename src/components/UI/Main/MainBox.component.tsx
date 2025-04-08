7f26050aa1e788aa8c9cc726241af891d49c1e08
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-04-08 18:51:42 -0400

    Changing background

--- src/components/UI/Main/MainBox.component.tsx
+++ src/components/UI/Main/MainBox.component.tsx
@@ -7,15 +7,14 @@ import Dashboard from "./Dashboard.component";
 const MainBoxComponent: React.FC = () =>{
 
     return(
-    
-        
+   
         <Routes>
                 <Route path ="/" element= {<Dashboard />}></Route>
                 {/* <Route path ="/" element ={<Navigate replace to="/home" />}/> */}
             </Routes>
 
 
-  )
+   )
 
 };
 
