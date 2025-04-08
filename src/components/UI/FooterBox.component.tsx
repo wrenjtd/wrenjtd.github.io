2516af83e857e98148278fa8c105f69a560fc682
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-04-08 18:09:55 -0400

    ""

--- src/components/UI/FooterBox.component.tsx
+++ src/components/UI/FooterBox.component.tsx
@@ -7,7 +7,7 @@ const FooterBoxComponent: React.FC = () => {
 
     return (
         <div>
-            <footer className="text-center">Copyright 2025. All rights reserved</footer>
+            <div className="text-center">Copyright 2025. All rights reserved</div>
             
 
         </div>)
