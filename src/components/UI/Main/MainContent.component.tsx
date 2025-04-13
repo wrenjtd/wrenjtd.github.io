3042760dcfc1e7c4a1173838b645e667590afb7c
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-04-13 15:37:36 -0400

    ""

--- src/components/UI/Main/MainContent.component.tsx
+++ src/components/UI/Main/MainContent.component.tsx
@@ -69,7 +69,7 @@ const MainContentComponent = () => {
                         <div className="flex flex-1 size-[20px]">
                         {item?.Response.displayProperties.icon && <img className="" src={`https://www.bungie.net/${item?.Response.displayProperties.icon}`} alt="misc img" />}
                         {item?.Response.displayProperties.icon && <img className="" src={`https://www.bungie.net/${item?.Response.displayProperties.icon}`} alt="weapon damage type" />}
-                        <p className="text-white ">2031</p>
+                        <p className="text-white size-auto ">2031</p>
                         </div>
 
                       </div>
