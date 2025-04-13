13fc33e5b3880e9c824495971da7871d197d6360
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-04-13 19:39:38 -0400

    Comment out manifest

--- src/Manifest.ts
+++ src/Manifest.ts
@@ -1,41 +1,41 @@
-import * as fs from 'fs';
-import Database from 'better-sqlite3';
+// import * as fs from 'fs';
+// import Database from 'better-sqlite3';
 
-/**
- * Class for accessing the manifest file. You will need to install <strong>sqlite3</strong> to use this class.
- */
-export default class Manifest {
-  private filepath: string;
-  private db;
+// /**
+//  * Class for accessing the manifest file. You will need to install <strong>sqlite3</strong> to use this class.
+//  */
+// export default class Manifest {
+//   private filepath: string;
+//   private db;
 
-  constructor(filepath: string) {
-    if (fs.existsSync(filepath)) {
-      this.filepath = filepath;
-      this.db = new Database(filepath);
-    } else {
-      throw new Error(
-        'The manifest file you want to refer to does not exist. Consider downloading it at first with Traveler#downloadManifest.'
-      );
-    }
-  }
+//   constructor(filepath: string) {
+//     if (fs.existsSync(filepath)) {
+//       this.filepath = filepath;
+//       this.db = new Database(filepath);
+//     } else {
+//       throw new Error(
+//         'The manifest file you want to refer to does not exist. Consider downloading it at first with Traveler#downloadManifest.'
+//       );
+//     }
+//   }
 
-  /**
-   * Query the manifest file with a valid SQlite query
-   * @async
-   * @param sqlLiteQuery SQlite valid query
-   * @return {Promise<object>} When fulfilled returns an object containing the result of the query
-   */
-  public queryManifest(sqlLiteQuery: string): Promise<object> {
-    return new Promise<object>((resolve, reject) => {
-      // this.db.serialize(() => {
-      //   this.db.all(sqlLiteQuery, (err, row) => {
-      //     if (err) {
-      //       reject(err);
-      //     } else {
-      //       resolve(row);
-      //     }
-      //   });
-      // });
-    });
-  }
-}
+//   /**
+//    * Query the manifest file with a valid SQlite query
+//    * @async
+//    * @param sqlLiteQuery SQlite valid query
+//    * @return {Promise<object>} When fulfilled returns an object containing the result of the query
+//    */
+//   public queryManifest(sqlLiteQuery: string): Promise<object> {
+//     return new Promise<object>((resolve, reject) => {
+//       // this.db.serialize(() => {
+//       //   this.db.all(sqlLiteQuery, (err, row) => {
+//       //     if (err) {
+//       //       reject(err);
+//       //     } else {
+//       //       resolve(row);
+//       //     }
+//       //   });
+//       // });
+//     });
+//   }
+// }
