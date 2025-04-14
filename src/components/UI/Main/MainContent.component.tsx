07e4db59533fbfab81dc54f89bd5ca88dece0c66
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-04-13 21:18:39 -0400

    flex box changes

--- src/components/UI/Main/MainContent.component.tsx
+++ src/components/UI/Main/MainContent.component.tsx
@@ -58,18 +58,18 @@ const MainContentComponent = () => {
                     <a href="#" className=" rounded-lg border border-gray-700 hover:border-white w-auto">
                       <div className="relative border-black rounded-lg border-1 size-full">
                         <div className="relative">
-                          {item?.displayProperties.icon && <img className="h-[50px] w-[50px]" src={`https://www.bungie.net/${item?.displayProperties.icon}`} alt="weapon icon" />}
+                          {item?.displayProperties.icon && <img className="h-[50px] w-[50px] rounded-t-lg" src={`https://www.bungie.net/${item?.displayProperties.icon}`} alt="weapon icon" />}
                           {item?.iconWatermark && <img className="h-[50px] w-[50px] absolute top-0 left-0" src={`https://www.bungie.net/${item?.iconWatermark}`} alt="icon watermark" />}
                         </div>
-                        <div className="flex flex-inital w-full h-[20px]">
+                        <div className="flex flex-inital w-full h-[20px] bg-black">
                           <div>
-                            {item?.displayProperties.icon && <img className="size-[8px]" src={`https://www.bungie.net/${item?.displayProperties.icon}`} alt="misc img" />}
+                            {item?.displayProperties.icon && <img className="size-[8px] " src={`https://www.bungie.net/${item?.displayProperties.icon}`} alt="misc img" />}
                           </div>
                           <div>
                             {item?.displayProperties.icon && <img className="size-[8px]" src={`https://www.bungie.net/${item?.displayProperties.icon}`} alt="weapon damage type" />}
                           </div>
                           <div>
-                            <p className="text-white text-xs ">2031</p>
+                            <p className="text-white text-xs text-center items-center ">2031</p>
                           </div>
                         </div>
 
@@ -91,11 +91,11 @@ const MainContentComponent = () => {
                     <a href="#" className=" rounded-lg border border-gray-700 hover:border-pink-600 w-auto">
                       <div className="relative border-black rounded-lg border-1 size-full">
                         <div className="relative">
-                          {item?.displayProperties.icon && <img className="h-[50px] w-[50px]" src={`https://www.bungie.net/${item?.displayProperties.icon}`} alt="weapon icon" />}
+                          {item?.displayProperties.icon && <img className="h-[50px] w-[50px] rounded-t-lg" src={`https://www.bungie.net/${item?.displayProperties.icon}`} alt="weapon icon" />}
                           {item?.iconWatermark && <img className="h-[50px] w-[50px] absolute top-0 left-0" src={`https://www.bungie.net/${item?.iconWatermark}`} alt="icon watermark" />}
                         </div>
 
-                        <div className="flex flex-inital w-full h-[20px]">
+                        <div className="flex flex-inital w-full h-[20px] bg-black">
                           <div>
                             {item?.displayProperties.icon && <img className="size-[8px]" src={`https://www.bungie.net/${item?.displayProperties.icon}`} alt="misc img" />}
                           </div>
@@ -103,7 +103,7 @@ const MainContentComponent = () => {
                             {item?.displayProperties.icon && <img className="size-[8px]" src={`https://www.bungie.net/${item?.displayProperties.icon}`} alt="weapon damage type" />}
                           </div>
                           <div>
-                            <p className="text-white text-xs ">2031</p>
+                            <p className="text-white text-xs text-center items-center ">2031</p>
                           </div>
                         </div>
                       </div>
