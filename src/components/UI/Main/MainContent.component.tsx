2050d60141d00f089897a2e80becc3e4076e5f7c
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-04-19 22:56:37 -0400

    Fix?

--- src/components/UI/Main/MainContent.component.tsx
+++ src/components/UI/Main/MainContent.component.tsx
@@ -136,7 +136,7 @@ const MainContentComponent = () => {
               <div>
                 <ul className="flex flex-col space-y-9">
 
-                  {user?.userWeapons[1].map((item, index) => (
+                  {user?.userWeapons[1]?.map((item, index) => (
 
                     <li className="flex" key={index}>
                       <a href="#" className="group relative rounded-lg border border-gray-700 hover:border-white w-auto transition-colors duration-150">
