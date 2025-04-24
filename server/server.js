2ae562f63d92f8c2b3a5608dd89a9b9796c8cbc4
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-04-24 18:36:16 -0400

    Example backend

--- server/server.js
+++ server/server.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const app = express();
+
+app.get("/api", (req, res)=>{
+    res.json({"fruits": ["Apple", "Orange", "Banana"]});
+});
+
+
+app.listen(8080, ()=>{
+    console.log("server started on port 8080");
+});
