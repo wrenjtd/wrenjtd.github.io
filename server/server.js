b40f655c1f5515439855f7206999ec153e7b2ec1
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-04-27 16:46:28 -0400

    Grab the manifest download

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { default: Manifest } = require("./Manifest");
 const corsOptions = {
     origin: ["https://wrenjtd.github.io"],
    
 };
-import(Manifest);
+
 
 app.use(cors(corsOptions) );
 
@@ -19,6 +18,3 @@ app.listen(8080, ()=>{
     console.log("server started on port 8080");
 });
 
-const manifest = new Manifest;
-
-manifest.downloadManifestJSON
