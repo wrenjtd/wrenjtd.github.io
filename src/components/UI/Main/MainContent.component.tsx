b6674377827d3d80197bf463eda5fc64f5f6a7f9
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-05-01 19:01:58 -0400

    ""

--- src/components/UI/Main/MainContent.component.tsx
+++ src/components/UI/Main/MainContent.component.tsx
@@ -48,7 +48,7 @@ const MainContentComponent = () => {
             {/* Character emblem + overlays */}
             <div className="m-2 grid grid-cols-3">
               
-              <div style={{ backgroundImage: `url('https://bungie.net${user.userCharacterProfiles?.Response.characters.data[Object.keys(user.userCharacterProfiles.Response.characters.data)[0]].emblemBackgroundPath}')` }} className="rounded-lg" >
+              <div style={{ backgroundImage: `url('https://bungie.net${user.userCharacterProfiles?.Response.characters.data[Object.keys(user.userCharacterProfiles.Response.characters.data)[0]].emblemBackgroundPath}')` }} className="rounded-lg h-12" >
               </div>
               
               <div style={{ backgroundImage: `url('https://bungie.net${user.userCharacterProfiles?.Response.characters.data[Object.keys(user.userCharacterProfiles.Response.characters.data)[1]].emblemBackgroundPath}')` }} className=" rounded-lg">
