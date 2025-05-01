c471563fce872eed9549131e297d3f11b817e22c
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-05-01 14:51:16 -0400

    inline style

--- src/components/UI/Main/MainContent.component.tsx
+++ src/components/UI/Main/MainContent.component.tsx
@@ -48,7 +48,11 @@ const MainContentComponent = () => {
             {/* Character emblem + overlays */}
             <div className="m-2 grid grid-cols-3">
               {/* <div className="flex justify-center bg-cover bg-center rounded-lg" > */}
-                <div className={`bg-[url('https://bungie.net/${user.userCharacterProfiles?.Response.characters.data[Object.keys(user.userCharacterProfiles.Response.characters.data)[0]].emblemBackgroundPath}')] bg-cover rounded-lg`}>
+                {/* <div className={`bg-[url('https://bungie.net/${user.userCharacterProfiles?.Response.characters.data[Object.keys(user.userCharacterProfiles.Response.characters.data)[0]].emblemBackgroundPath}')] bg-cover rounded-lg`} >
+
+                </div> */}
+
+                <div style={{ backgroundImage: `url('https://bungie.net/${user.userCharacterProfiles?.Response.characters.data[Object.keys(user.userCharacterProfiles.Response.characters.data)[1]].emblemBackgroundPath}')` }} className="bg-cover rounded-lg" >
 
                 </div>
               {/* </div> */}
