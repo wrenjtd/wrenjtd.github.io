66448e2d6251b7f29fd96668edcc89a9141f7a5f
wrenjtd <51337097+wrenjtd@users.noreply.github.com>
2025-05-01 22:07:42 -0400

    ""

--- src/components/UI/Main/Dashboard.component.tsx
+++ src/components/UI/Main/Dashboard.component.tsx
@@ -6,15 +6,10 @@ import { Routes, Route } from "react-router-dom";
 import PickerComponent from "./Picker.component";
 
 export default function Dashboard() {
-
-
-
-
   return (
-
-    <div>
-      <HeaderContentComponent ></HeaderContentComponent>
-      <div className="flex flex-col min-h-screen bg-gray-800">
+    <div className="flex flex-col min-h-screen">
+      <HeaderContentComponent />
+      <div className="flex-grow bg-gray-800">
 
         <Routes>
           <Route path="/" element={<MainContentComponent />}></Route>
